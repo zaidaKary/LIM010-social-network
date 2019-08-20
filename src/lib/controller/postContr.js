4d6c2da0dcdc328b8241671520392326fe9f895a
Betsycaceres <betsy0357@gmail.com>
2019-08-20 16:46:02 -0500

    resolviendo conflictos

--- src/lib/controller/postContr.js
+++ src/lib/controller/postContr.js
@@ -3,32 +3,20 @@ import { addPostFirebase } from '../model/modelPost.js';
 import { db } from '../../main.js';
 
 export const savePost = (event) => {
-  event.preventDefault();
-  const notePost = document.querySelector('#publicacion').value;
-  console.log(notePost);
-  const user = userCurrent();
-  addPostFirebase(notePost, user) // pinta en el home
-    .then(() => {
-      // console.log('Document written with ID: ', docRef.id);
-    })
-    .catch(() => {
-      console.error('Error adding document: ', error);
+    event.preventDefault();
+    const notePost = document.querySelector('#publicacion').value;
+    console.log(notePost);
+    const user = userCurrent();
+    console.log(user);
+    addPostFirebase(notePost, user) // pinta en el home
+        .then(() => {
+            // console.log('Document written with ID: ', docRef.id);
+        })
+        .catch(() => {
+            console.error('Error adding document: ', error);
+            userCorreo.value = doc.data().Email;
+        });
 
-<<<<<<< HEAD
-    });
-
-  const postCreados = document.getElementById('listOfPos');
-  db.collection('post').onSnapshot((querySnapshot) => {
-    postCreados.innerHTML = '';
-    querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-      // doc.data() is never undefined for query doc snapshots
-      postCreados.innerHTML += `
-            <tr>
-            <th scope="col">${doc.id}</th>
-            <th scope="col">${doc.data().notes}</th>
-            <td id = "delete-post"><button id="${doc.id}" name="delete" class="compartir" >Eliminar</button></td>
-=======
     const postCreados = document.getElementById('listOfPos');
     db.collection('post').onSnapshot((querySnapshot) => {
         postCreados.innerHTML = '';
@@ -41,19 +29,18 @@ export const savePost = (event) => {
             <th scope="col">${doc.data().email}</th>
             <th scope="col">${doc.data().notes}</th>
             <td><button id="${doc.data().id}" name="delete" class="compartir" >Eliminar</button></td>
->>>>>>> 5ddc035d10a47551142fa2419ea170917e3dd996
           </tr>`;
 
+        });
     });
-  });
 };
-
-
 export const deletePost = (id) => {
-  deletePostFirebase(id)
-    .then(() => {
-      // console.log('Document written with ID: ', docRef.id);
-    }).catch(() => {
-      console.error('Error adding document: ', error);
-    });
-};
+    //event.preventDefault();
+    // const id = event.target.id;
+    deletePostFirebase(id)
+      .then(() => {
+        // console.log('Document written with ID: ', docRef.id);
+      }).catch(() => {
+        console.error('Error adding document: ', error);
+      });
+  };
