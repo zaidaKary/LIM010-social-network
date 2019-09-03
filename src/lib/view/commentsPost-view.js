2d20d8d4f6903eb2682474d0828b05b76cd3dd66
zaidaKary <zaida.260194@gmail.com>
2019-09-03 16:16:34 -0500

    corrigiendo el email del comentario

--- src/lib/view/commentsPost-view.js
+++ src/lib/view/commentsPost-view.js
@@ -1,9 +1,9 @@
-
+import { userCurrent } from '../model/modelLoginRegistro.js';
 export const itemComment = (objComment) => {
   const divElement = document.createElement('div');
   divElement.innerHTML = `
     <div class="comentario-post">
-    <p class="color-post">${objComment.email}</p>
+    <p class="color-post">${userCurrent().email}</p>
     <p >${objComment.comment}</p>
     </div>    
     `;
